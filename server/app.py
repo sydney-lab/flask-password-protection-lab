9b87d94a3e14e53c97f54f84f1c5d5e2aa743739
Sydney <sydneyosindi03@gmail.com>
2026-08-12 23:26:50 +0300

    final solution

--- server/app.py
+++ server/app.py
@@ -7,15 +7,49 @@ from config import app, db, api
 from models import User, UserSchema
 
 class ClearSession(Resource):
-
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
+        return {}, 204
 
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User(username=data['username'])
+        user.password_hash = data['password']
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        return UserSchema().dump(user), 201
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return UserSchema().dump(user), 200
+        return {}, 204
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username']).first()
+        if user and user.authenticate(data['password']):
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 200
+        return {'error': 'Invalid credentials'}, 401
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
         return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,19 +10,16 @@ class User(db.Model):
     username = db.Column(db.String)
     _password_hash = db.Column(db.String)
 
-    # Build method to protect password_hash property
     @hybrid_property
     def password_hash(self):
-        pass
+        raise Exception('Password hashes may not be viewed.')
 
-    # Build method to set password hash property using bcrypt.generate_password_hash()
     @password_hash.setter
     def password_hash(self, password):
-        pass
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
-    # Build authenticate method that uses bcrypt.check_password_hash()
     def authenticate(self, password):
-        pass
+        return bcrypt.check_password_hash(self._password_hash, password)
 
     def __repr__(self):
         return f'User {self.username}, ID: {self.id}'
